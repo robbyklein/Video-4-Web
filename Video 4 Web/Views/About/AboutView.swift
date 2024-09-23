031c1fc236159867e3e903f191a7c65932b767f8
Robby Klein <hello@robbyk.com>
2024-09-22 17:05:14 -0700

    Adjust about page.

--- Video 4 Web/Views/About/AboutView.swift
+++ Video 4 Web/Views/About/AboutView.swift
@@ -17,9 +17,11 @@ struct AboutView: View {
 
             Spacer().frame(height: 5)
 
-            Text("Version 1.0.0")
-                .font(.subheadline)
-                .foregroundColor(.secondary)
+            if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+                Text("Version \(version)")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
 
             Spacer().frame(height: 20)
 
@@ -27,7 +29,7 @@ struct AboutView: View {
                 """
                 [Video 4 Web](https://video4web.robbyk.com) by [Robert Klein](https://robbyk.com) \
                 is a GUI for [FFmpeg](https://www.ffmpeg.org/) and was inspired by \
-                [ImageOptim](https://imageoptim.com/) by Kornel Lesiński and contributors.
+                [ImageOptim](https://imageoptim.com/).
 
                 Video 4 Web can be redistributed and modified under \
                 [GNU General Public License version 2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.html) or later.
